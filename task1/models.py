3a1a90f8bb8e8bb5062a614b35ca68046464d549
master <masterbe2012@yandex.ru>
2024-10-21 22:47:21 +0300

    Второй заполнили таблицы в db прописали связи

--- task1/models.py
+++ task1/models.py
@@ -12,4 +12,5 @@ class Game(models.Model):
     cost = models.DecimalField(max_digits=10 , decimal_places=2)
     size = models.DecimalField(max_digits=10 , decimal_places=2)
     age_limited  = models.BooleanField(default=False)
+    description = models.TextField()
     buyer = models.ManyToManyField(Buyer, related_name='games')
